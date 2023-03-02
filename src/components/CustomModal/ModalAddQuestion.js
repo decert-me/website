981c414ee1aded6fad21e2bde5882ab9894e9330
Caoqizhi <2391580279@qq.com>
2023-03-02 16:32:46 +0800

    fix: #I6J0IX

--- src/components/CustomModal/ModalAddQuestion.js
+++ src/components/CustomModal/ModalAddQuestion.js
@@ -32,6 +32,10 @@ export default function ModalAddQuestion(props) {
 
         let optionArr = [];
         const { type, ans } = filterType(values);
+        if (typeof ans !== 'number') {
+            message.warning("请选择至少一个正确答案")
+            return
+        }
         values.options.map((e)=>{
             optionArr.push(e.title)
         })
