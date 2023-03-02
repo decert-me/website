cbc9224ccf3de416f27150e5e2b9842ff911e54a
Caoqizhi <2391580279@qq.com>
2023-03-02 17:29:27 +0800

    fix: 修复百分比bug

--- src/utils/GetPercent.js
+++ src/utils/GetPercent.js
@@ -1,7 +1,7 @@
 
 
 export const GetPercent = (total, score) => {
-    let num = (100 / total);
+    let num = (score / total);
     let percent = Number(num.toString().match(/^\d+(?:\.\d{0,2})?/)) * score;
     return percent
 }
