06d8b6f15c5e4218f103c6e5462a0c28683c1535
Caoqizhi <2391580279@qq.com>
2023-03-14 10:37:40 +0800

    en: 完成编辑资料

--- src/utils/UploadProps.js
+++ src/utils/UploadProps.js
@@ -1,5 +1,5 @@
 import { message } from "antd";
-import { ipfsImg } from "../request/api/public";
+import { ipfsImg, uploadAvatar } from "../request/api/public";
 
 export const UploadProps = {
     name: "file",
@@ -57,4 +57,62 @@ export const UploadProps = {
         message.error("Image must smaller than 20MB!");
       }
     }
+};
+
+export const UploadAvatarProps = {
+  name: "file",
+  multiple: false,
+  maxCount: 1,
+  onStart(file) {
+    console.log('onStart ==>', file);
+  },
+  onError(err) {
+    console.log("onError ==>", err);
+  },
+  customRequest({
+    data,
+    file,
+    filename,
+    onError,
+    onSuccess
+  }) {
+    const formData = new FormData();
+    if (data) {
+      Object.keys(data).forEach(key => {
+        formData.append(key, data[key]);
+      });
+    }
+    formData.append('file', file);
+    uploadAvatar(formData)
+    .then(res => {
+      console.log('success ==>',res , file);
+      onSuccess(res, file);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+    return {
+      abort() {
+        console.log("upload progress is aborted.");
+      }
+    };
+  },
+  beforeUpload(file) {
+    const formatArr = ["image/jpeg","image/png","image/svg+xml","image/gif"]
+    let isImage = false
+    formatArr.map((e)=>{
+      if ( file.type === e ) {
+        isImage = true
+      }
+    })
+    if (!isImage) {
+      message.error("You can only upload JPG/PNG file!");
+      return false
+    }
+    const isLt4M = file.size / 1024 / 1024 < 4;
+    if (!isLt4M) {
+      message.error("Image must smaller than 4MB!");
+    }
+  }
 };
--- src/views/User/edit.js
+++ src/views/User/edit.js
@@ -1,5 +1,19 @@
+import "@/assets/styles/view-style/user-edit.scss"
+import { Button, Input, message, Upload } from "antd"
+import {
+    PlusOutlined,
+    LoadingOutlined,
+    // PlusOutlined
 
-
+} from '@ant-design/icons';
+import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+import { useLocation, useNavigate } from "react-router-dom";
+import { hashAvatar } from "@/utils/HashAvatar";
+import { NickName } from "@/utils/NickName";
+import { getUser, putUser } from "@/request/api/public";
+import { UploadAvatarProps } from "@/utils/UploadProps";
+const { TextArea } = Input;
 
 export default function UserEdit(params) {
     
@@ -10,11 +24,173 @@ export default function UserEdit(params) {
      * 3. 回显
      * 4. 提交
      */
+    const { address } = useAccount();
+    const location = useLocation();
+    const navigateTo = useNavigate();
 
+    let [account, setAccount] = useState();
+    let [user, setUser] = useState();
+    let [info, setInfo] = useState();
+    let [loading, setLoading] = useState(false);
+
+    const getBase64 = (img, callback) => {
+        const reader = new FileReader();
+        reader.addEventListener('load', () => callback(reader.result));
+        reader.readAsDataURL(img);
+    };
+
+    const handleChange = (info) => {
+        if (info.file.status === 'uploading') {
+          setLoading(true);
+          return;
+        }
+        if (info.file.status === 'done') {
+          // Get this url from response in real world.
+          getBase64(info.file.originFileObj, (url) => {
+            setLoading(false);
+            // setImageUrl(url);
+            user.avatar = info.file.response.data.url;
+            setUser({...user});
+            info.avatar = url;
+            setInfo({...info});
+          });
+        }
+    };
+
+    const goBack = () => {
+        navigateTo(`/user/${account}`);
+    }
+
+    const changeUser = (value, key) => {
+        user[key] = value;
+        setUser({...user});
+    }
+
+    const save = () => {
+        putUser(user)
+        .then(res => {
+            if (res) {
+                message.success('保存成功!');
+                setTimeout(() => {
+                    goBack();
+                }, 1000);
+            }
+        })
+    }
+
+    const verify = async() => {
+        return new Promise((resolve, reject) => {
+            account = location?.pathname?.split("/").slice(-1)[0];
+            setAccount(account);
+            let isMe = account === address;
+            resolve(isMe)
+        })
+    }
+
+    const init = async() => {
+        if (!await verify()) {
+            goBack();
+            return
+        }
+        await getUser({address: account})
+        .then(res => {
+            user = res.data ? res.data : {};
+            setUser({...user});
+        })
+        console.log(user);
+        info = {
+            nickname: user?.nickname ? user.nickname : NickName(account),
+            address: account,
+            description: user?.description ? user.description : "暂无介绍",
+            avatar: user?.avatar ? process.env.REACT_APP_BASE_URL + user.avatar : hashAvatar(account)
+        }
+        setInfo({...info});
+        console.log(info);
+    }
+
+    useEffect(() => {
+        init();
+    },[])
 
     return (
+        info &&
         <div className="UserEdit">
-            <h1>xxx</h1>
+            <div className="UserEdit-inner">
+                <div className="title">
+                    编辑资料
+                </div>
+                <div className="content">
+                    <div className="avatar">
+                        <div className="img">
+                            <img src={info.avatar} alt="" />
+                        </div>
+                        <Upload
+                            name="avatar"
+                            listType="picture-circle"
+                            className="avatar-uploader"
+                            showUploadList={false}
+                            {...UploadAvatarProps} 
+                            onChange={handleChange}
+                        >
+                            <Button className="btn">
+                                
+                                {
+                                    loading ? 
+                                    <>
+                                        <LoadingOutlined />正在上传
+                                    </>
+                                    :
+                                    <>
+                                        <PlusOutlined />上传头像
+                                    </>
+                                }
+                            </Button>
+                        </Upload>
+                    </div>
+                    <div className="inner">
+                        <p className="label">名称</p>
+                        <Input 
+                            className="input" 
+                            placeholder="0x0a...9e57" 
+                            maxLength={16} 
+                            defaultValue={info.nickname}
+                            onChange={(e) => changeUser(e.target.value, 'nickname')}
+                        />
+                    </div>
+                    <div className="inner">
+                        <p className="label">简介</p>
+                        <TextArea
+                            showCount
+                            maxLength={100}
+                            className="textarea"
+                            defaultValue={info.description}
+                            onChange={(e) => changeUser(e.target.value, 'description')}
+                            style={{
+                                height: 120,
+                                resize: 'none',
+                            }}
+                            // onChange={onChange}
+                            placeholder="请介绍你的兴趣或经验..."
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="UserEdit-btns">
+                <Button 
+                    danger 
+                    className="btn cancel"
+                    onClick={goBack}
+                >
+                    取消
+                </Button>
+                <Button 
+                    type="primary" 
+                    className="btn save"
+                    onClick={() => save()}
+                >
+                    保存
+                </Button>
+            </div>
         </div>
     )
 }
