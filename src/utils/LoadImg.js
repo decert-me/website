318691444c629b4bf3a5ea0f974259ece203a2a4
Caoqizhi <2391580279@qq.com>
2023-03-21 15:19:13 +0800

    en: 修改网管配置

--- src/utils/LoadImg.js
+++ src/utils/LoadImg.js
@@ -7,7 +7,7 @@ export function loadImg() {
     return Promise.race(requests.map((request) =>
       request.catch(() => Promise.resolve())
     )).then((response) => {
-      if (response) {
+      if (response.status === 200) {
         const gateway = response.url.slice(0, -imgUrl.length);
         console.log(`Using gateway: ${gateway}`);
         return gateway;
