15af7f13efffad1b3fe34fe14e11088eb2ba26c0
Caoqizhi <2391580279@qq.com>
2023-04-10 14:41:25 +0800

    fix: 修复cert页ens-user显示

--- src/views/Cert.js
+++ src/views/Cert.js
@@ -176,7 +176,14 @@ export default function Cert(params) {
                 <div className="Cert-sidbar">
                     <CertSearch />
                     <Divider className="divider"  />
-                    <CertUser account={addr} ensName={ens ? ens : accountEns} status={status} />
+                    {
+                        addr && 
+                        <CertUser 
+                            account={addr} 
+                            ensName={ens ? ens : accountEns} 
+                            status={status} 
+                        />
+                    }
                     <div className="mt50"></div>
                     <CertNfts 
                         account={accountAddr} 
