cad1cc3144586a6901b2970586c131bd2df3111a
Caoqizhi <2391580279@qq.com>
2023-04-11 09:05:07 +0800

    del: beforerouter store

--- src/components/BeforeRouterEnter/index.js
+++ src/components/BeforeRouterEnter/index.js
@@ -1,6 +1,4 @@
 import DefaultLayout from "@/containers/DefaultLayout";
-import store from "@/redux/store";
-import { Provider } from 'react-redux';
 
 export default function BeforeRouterEnter() {
     return (
