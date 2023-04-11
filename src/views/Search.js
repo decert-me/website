f7658d5dafdd67d4d6f117b05f595de81e50becc
Caoqizhi <2391580279@qq.com>
2023-04-11 15:01:38 +0800

    en: ens to address

--- src/views/Search.js
+++ src/views/Search.js
@@ -11,8 +11,8 @@ import { useTranslation } from "react-i18next";
 
 export default function Search(params) {
 
-    // let [account, setAccount] = useState("tinyxiong.eth");
-    let [account, setAccount] = useState("0xD6823f807C45eFDC56c9aE8Db0226CA10af6E8AB");
+    let [account, setAccount] = useState("tinyxiong.eth");
+    // let [account, setAccount] = useState("0xD6823f807C45eFDC56c9aE8Db0226CA10af6E8AB");
     
     const { t } = useTranslation("cert");
     const navigateTo = useNavigate();
@@ -25,6 +25,12 @@ export default function Search(params) {
     }
 
     const start = async() => {
+        if (account === "tinyxiong.eth") {
+            setTimeout(() => {
+                navigateTo(`/0xD6823f807C45eFDC56c9aE8Db0226CA10af6E8AB`)
+            }, 500);
+            return
+        }
         const ensAddress = await verify();
         if (ensAddress) {
             setTimeout(() => {
