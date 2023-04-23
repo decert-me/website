730faa6d523028da5c74712438370f35b97f7045
Caoqizhi <2391580279@qq.com>
2023-04-23 16:55:41 +0800

    en: env-example update

--- src/components/Cert/ModalAddSbt.js
+++ src/components/Cert/ModalAddSbt.js
@@ -4,13 +4,11 @@ import {
 } from '@ant-design/icons';
 import "@/assets/styles/component-style/cert/modal-addsbt.scss"
 import AddSbt from "./AddSbt";
-import { useNavigate } from "react-router-dom";
 import MyContext from "@/provider/context";
 import { useContext } from "react";
 
 export default function ModalAddSbt(props) {
     const { isModalOpen, handleCancel } = props;
-    const navigateTo = useNavigate();
     const { isMobile } = useContext(MyContext);
 
     return (
