02f0a2f86e6cf92bccf31ea4add484d2967b07a1
Caoqizhi <2391580279@qq.com>
2023-04-26 18:02:28 +0800

    en: 完成claim页挑战失败显示

--- src/components/CustomChallenge/CustomClaimInfo.js
+++ src/components/CustomChallenge/CustomClaimInfo.js
@@ -1,7 +1,7 @@
-import { Divider, Progress } from "antd"
+import { Button, Divider, Progress } from "antd"
 import { Viewer } from "@bytemd/react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import ModalViewRecommed from "../CustomModal/ModalViewRecommed";
 import { useContext, useMemo } from "react";
 import pluginGfm from '@bytemd/plugin-gfm'
@@ -9,6 +9,7 @@ import frontmatter from '@bytemd/plugin-frontmatter'
 import breaks from '@bytemd/plugin-breaks'
 import highlight from '@bytemd/plugin-highlight-ssr'
 import MyContext from "@/provider/context";
+import CustomViewer from "../CustomViewer";
 
 export default function CustomClaimInfo(props) {
     
@@ -16,6 +17,11 @@ export default function CustomClaimInfo(props) {
     const { t } = useTranslation(["claim", "translation"]);
     const plugins = useMemo(() => [pluginGfm(),frontmatter(),highlight(),breaks()], [])
     const { isMobile } = useContext(MyContext);
+    const navigateTo = useNavigate();
+
+    function reChallenge(params) {
+        navigateTo(`/quests/${detail.tokenId}`);
+    }
 
     return (
         <>
@@ -37,7 +43,6 @@ export default function CustomClaimInfo(props) {
                     </div>
                     <div className="score">
                         <p className="network">{detail.title}</p>
-                        <h4>{t("score.now")}</h4>
                         <p className="pass">{t("score.passScore",{score: answerInfo.passingPercent})}</p>
                         <div className="score-detail">
                             <div className="circle">
@@ -83,46 +88,39 @@ export default function CustomClaimInfo(props) {
                             :
                                 <p className="title">{t("unpass")}</p>
                         }
-                        {
+                    </div>
+                    <div className="score">
+                        <div className="circle">
+                            <Progress
+                                type="circle"
+                                percent={percent}
+                                width={230}
+                                format={(percent) => percent}
+                                strokeWidth={3}
+                            />
+                        </div>
+                        <div className="info">
+                            <p className="network">{detail.title}</p>
+                            <p className="pass">{t("score.passScore",{score: answerInfo.passingPercent})}</p>
+                            <Button className="btn" onClick={reChallenge}>
+                                {t("translation:btn-go-challenge")}
+                            </Button>
+                        </div>
+                    </div>
+                    {
                             !answerInfo.isPass && detail.recommend && !isClaim ? 
                             <div className="viewer" >
                                 <div className="viewer-head">
                                     <p>{t("recommend")}</p>
-                                    <ModalViewRecommed
-                                        text={JSON.parse(detail.recommend)} 
-                                        plugins={plugins}
-                                        viewText={t("view")}
-                                    />
-                
                                 </div>
                                 <Divider />
                                 <div className="viewer-content">
-                                    <Viewer value={JSON.parse(detail.recommend)} plugins={plugins} />
+                                    <CustomViewer label={JSON.parse(detail.recommend)} />
                                 </div>
                             </div>
                             :
                             <p className="desc">{t("desc")}</p>
-                        }
-                    </div>
-                    <div className="score">
-                        <p className="network">{detail.title}</p>
-                        <h4>{t("score.now")}</h4>
-                        <p className="pass">{t("score.passScore",{score: answerInfo.passingPercent})}</p>
-                        <div className="score-detail">
-                            <div className="circle">
-                                <Progress
-                                    type="circle"
-                                    percent={percent}
-                                    width={172}
-                                    format={(percent) => percent}
-                                    strokeWidth={6}
-                                />
-                            </div>
-                            <Link className="btn" to={`/quests/${detail.tokenId}`}>
-                                <button className="btn">{t("translation:btn-go-challenge")}</button>
-                            </Link>
-                        </div>
-                    </div>
+                    }
                 </div>
             }
         </>
