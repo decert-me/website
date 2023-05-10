d4bddd8732547bf21f9672774d867965be7a875a
Caoqizhi <2391580279@qq.com>
2023-05-10 14:34:21 +0800

    en: 修改monaco js包下载路径

--- src/components/MonacoEditor/MonacoComponent.js
+++ src/components/MonacoEditor/MonacoComponent.js
@@ -1,13 +1,10 @@
-import { useEffect, useState } from "react";
-import Editor, { loader } from "@monaco-editor/react";
+import Editor from "@monaco-editor/react";
 import { LoadingOutlined } from '@ant-design/icons';
 import { useMonacoInit } from "@/hooks/useMonacoInit";
 
 
 export default function MonacoComponent(props) {
     const { value, onChange, language } = props;
-    const { config, init } = loader;
-    let [editorIsOk, setEditorIsOk] = useState();
     const { languageInit } = useMonacoInit({
         language: language
     })
@@ -16,23 +13,7 @@ export default function MonacoComponent(props) {
         minimap: { enabled: false },  // 隐藏侧边栏
     };
 
-    async function monacoInit(params) {
-        config({
-            paths: {
-                vs: "https://unpkg.com/monaco-editor@0.36.1/min/vs"
-            },
-            // monaco: monaco
-        })
-        await init();
-        setEditorIsOk(true);
-    }
-
-    useEffect(() => {
-        monacoInit();
-    },[])
-
     return (
-        editorIsOk &&
         <Editor
             // width="800"
             height="300px"
