f0c1c86d9e4104a7a357a47b710227c61252937c
Caoqizhi <2391580279@qq.com>
2023-05-24 11:20:00 +0800

    en: 完成部分样式

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,10 +1,14 @@
 import { Button, message } from "antd";
+import {
+    TwitterOutlined
+} from '@ant-design/icons';
 import { useNavigate } from "react-router-dom";
 import "@/assets/styles/view-style/index.scss"
 import "@/assets/styles/mobile/view-style/index.scss"
 import { useTranslation } from "react-i18next";
 import MyContext from "@/provider/context";
 import { useContext } from "react";
+import CustomIcon from "@/components/CustomIcon";
 
 export default function Index(params) {
     
@@ -24,35 +28,98 @@ export default function Index(params) {
         <div className="Home ">
             <div className="main">
                 <div className="main-center">
-                <div className="weight-info">
-                    <h3>DeCert.me</h3>
-                    {/* describe */}
-                    <div className="describe">
-                    <p>{t("home.slogan1")}</p>
-                    </div>
-                    {/* text */}
-                    <div className="text">
-                    <p>{t("home.slogan2")}</p>
-                    </div>
-                    {/* Explore challenge */}
-                    <div className="mar" style={{justifyContent: "center"}}>
-                    <Button
-                        className="challenge-btn"
-                        onClick={() => {
-                            navigateTo("/challenges");
-                        }}
-                    >
-                        <span className="btn">{t("home.btn-explore")}</span>
-                    </Button>
-                    {/* Creative a challenge */}
-                    <Button
-                        className="creative-btn"
-                        onClick={() => goCreate()}
-                    >
-                        <span className="creative">{t("home.btn-publish")}</span>
-                    </Button>
-                    </div>
-                </div>
+                        <h1 className="describe">{t("home.slogan1")}</h1>
+                        <h2 className="text">{t("home.slogan2")}</h2>
+
+                        {/* social */}
+                        <div className="social">
+                            <div className="social-item">
+                                <TwitterOutlined />
+                            </div>
+                            <div className="social-item">
+                                <CustomIcon type="icon-discord" />
+                            </div>
+                        </div>
+
+                        {/* list */}
+                        <div className="listImg">
+
+                        </div>
+
+                        {/* 愿景 */}
+                        <div className="vision">
+                            <h3>我们的愿景</h3>
+                            <p>
+                                让每个 Builder 在&nbsp;<span>Web3.0</span>
+                                &nbsp;时代都拥有可信的履历数据
+                            </p>
+                            <p>
+                                形成一个更自由、更可信、更高效的 Builder 世界。
+                            </p>
+                        </div>
+
+                        <div className="intro">
+                            <div className="linear-bg"></div>
+                            {/* ai */}
+                            <div className="ai intro-content">
+                                <div className="content-l">
+                                    <h2>AI时代的教育方式</h2>
+                                    <ul>
+                                        <li>
+                                            <h3>全民1对1辅导</h3>                                            
+                                            <p>通过AI，可以为所有学员带来一对一的辅导，让学员有更深刻的理解、自信</p>
+                                        </li>
+                                        <li>
+                                            <h3>AI助教</h3>                                            
+                                            <p>AI帮助教育工作者制作更好的课程计划，跟踪学生反馈，提升教学水平</p>
+                                        </li>
+                                        <li>
+                                            <h3>专家训练</h3>                                            
+                                            <p>AI由不同领域专家训练，确保质量和准确性</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="content-r">
+                                    
+                                </div>
+                            </div>
+
+                            {/* sbt */}
+                            <div className="sbt intro-content">
+                                <div className="content-l"></div>
+                                <div className="content-r">
+                                    <h2>SBT - 学习成果证明</h2>
+                                    <h2>Proof of Learn</h2>
+                                    <ul>
+                                        <li>履历认证</li>
+                                        <li>DID 社交图谱</li>
+                                        <li>多维度履历证明</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* 合作伙伴 */}
+                        <div className="partner">
+                            <p>合作伙伴</p>
+                            <div className="partner-item">
+                                
+                            </div>
+                            <div className="partner-item">
+                                
+                            </div>
+                            <div className="partner-item">
+                                
+                            </div>
+                            <div className="partner-item">
+                                
+                            </div>
+                        </div>
+
+                        {/* 贡献者 */}
+                        <div className="contributor">
+
+                        </div>
                 </div>
             </div>
         </div>
