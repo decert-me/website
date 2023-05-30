ca16169788a6acf7d017b0115e689ba9b7d0cceb
Caoqizhi <2391580279@qq.com>
2023-05-30 16:05:03 +0800

    en: 添加测试用例示例提示

--- src/components/Publish/CustomCase.js
+++ src/components/Publish/CustomCase.js
@@ -61,13 +61,31 @@ function CustomCase(props, ref) {
         console.log(caseArr);
         // TODO: 节流
     }
+
+    function exampleAction(dom, type) {
+        const img = document.querySelector(dom);
+        img.style.display = type;
+        const { top } = document.querySelector(".hover1").getBoundingClientRect();
+        img.style.bottom = document.documentElement.offsetHeight - top + 50 + "px";
+        img.style.left = (document.documentElement.offsetWidth - img.offsetWidth) / 2 + "px";
+
+        console.log(document.documentElement.offsetWidth);
+    }
     
     const items = [
         {
             key: '1',
+            className: "hover1",
             label: (
                 <p onClick={() => addCase()}>
-                    测试用例
+                    测试用例 
+                    <span
+                        onMouseEnter={() => exampleAction(".show1", "block")}
+                        onMouseLeave={() => exampleAction(".show1", "none")}
+                    >
+                        ❓
+
+                    </span>
                 </p>
             ),
         },
@@ -79,71 +97,85 @@ function CustomCase(props, ref) {
             label: (
                 <p onClick={() => addCase("special")}>
                     代码测试用例
+                    <span
+                        onMouseEnter={() => exampleAction(".show2", "block")}
+                        onMouseLeave={() => exampleAction(".show2", "none")}
+                    >
+                        ❓
+                    </span>
                 </p>
             ),
         }
     ];
 
     return (
-        <div className="customCase">
-            {contextHolder}
-            {
-                caseArr.map((e,i) => 
-                    "spj_code" in e ?
-                        // 特殊题
-                        <CodingSpecial
-                            key={i} 
-                            onChange={
-                                (e, key) => changeValue(e, "spj_code", i, key)
-                            } 
-                            defaultValue={e}
-                            deleteCase={() => deleteCase(i)}
-                            checkCode={async() => {
-                                return await checkCode({
-                                    spj_code: [e.spj_code]
-                                })
-                            }}
-                        /> 
-                        :
-                        // 普通题
-                        <Coding 
-                            key={i}  
-                            onChange={
-                                (e, type) => changeValue(e, type, i)
-                            } 
-                            defaultValue={e}
-                            deleteCase={() => deleteCase(i)}
-                            checkCode={async() => {
-                                return await checkCode({
-                                    input: [],
-                                    example_input: [e.input],
-                                    example_output: [e.output]
-                                })
-                            }}
-                        />
-                )
-            }
-            <Dropdown
-                menu={{
-                    items,
-                }}
-                overlayClassName="customCaseMenu"
-                placement="bottom"
-                trigger="click"
-                arrow={{
-                    pointAtCenter: true,
-                }}
-            >
-                <Button 
-                    className="addCase" 
-                    type="dashed" 
-                    icon={<PlusOutlined />}
-                    block
+        <>
+            <div className="customCase">
+                {contextHolder}
+                {
+                    caseArr.map((e,i) => 
+                        "spj_code" in e ?
+                            // 特殊题
+                            <CodingSpecial
+                                key={i} 
+                                onChange={
+                                    (e, key) => changeValue(e, "spj_code", i, key)
+                                } 
+                                defaultValue={e}
+                                deleteCase={() => deleteCase(i)}
+                                checkCode={async() => {
+                                    return await checkCode({
+                                        spj_code: [e.spj_code]
+                                    })
+                                }}
+                            /> 
+                            :
+                            // 普通题
+                            <Coding 
+                                key={i}  
+                                onChange={
+                                    (e, type) => changeValue(e, type, i)
+                                } 
+                                defaultValue={e}
+                                deleteCase={() => deleteCase(i)}
+                                checkCode={async() => {
+                                    return await checkCode({
+                                        input: [],
+                                        example_input: [e.input],
+                                        example_output: [e.output]
+                                    })
+                                }}
+                            />
+                    )
+                }
+                <Dropdown
+                    menu={{
+                        items,
+                    }}
+                    overlayClassName="customCaseMenu"
+                    placement="bottom"
+                    trigger="click"
+                    arrow={{
+                        pointAtCenter: true,
+                    }}
                 >
-                    添加测试用例
-                </Button>
-            </Dropdown>
-        </div>
+                    <Button 
+                        className="addCase" 
+                        type="dashed" 
+                        icon={<PlusOutlined />}
+                        block
+                    >
+                        添加测试用例
+                    </Button>
+                </Dropdown>
+            </div>
+            <div className="poa  show1">
+                <img src={require("@/assets/images/img/publish-example1.png")} alt="" />
+            </div>
+            <div className="poa  show2">
+                <img src={require("@/assets/images/img/publish-example2.png")} alt="" />
+            </div>
+        </>
     )
 }
 
