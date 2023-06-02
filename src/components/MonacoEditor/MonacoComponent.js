0db2f55d7ce9aa7b0603b4a8d822648f2214b049
Caoqizhi <2391580279@qq.com>
2023-06-02 15:38:51 +0800

    en: 【monaco】移动端开启自动换行

--- src/components/MonacoEditor/MonacoComponent.js
+++ src/components/MonacoEditor/MonacoComponent.js
@@ -1,16 +1,20 @@
 import Editor from "@monaco-editor/react";
 import { LoadingOutlined } from '@ant-design/icons';
 import { useMonacoInit } from "@/hooks/useMonacoInit";
+import { useContext } from "react";
+import MyContext from "@/provider/context";
 
 
 export default function MonacoComponent(props) {
     const { value, onChange, language, height } = props;
+    const { isMobile } = useContext(MyContext);
     const { languageInit } = useMonacoInit({
         language: language
     })
     
     const options = {
         minimap: { enabled: false },  // 隐藏侧边栏
+        wordWrap: isMobile ? "on" : "off"
     };
 
     return (
