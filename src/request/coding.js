b048de292bd90a49e0f06dbe54140c4ac7dc655d
Caoqizhi <2391580279@qq.com>
2023-07-24 16:50:19 +0800

    del: 移除REACT_APP_IPFS_TIMEOUT

--- src/request/coding.js
+++ src/request/coding.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // Client-side-only code
 let codingAxios = axios.create({
   baseURL: process.env.REACT_APP_CODE_BASE_URL,
-  timeout: process.env.REACT_APP_IPFS_TIMEOUT + 60000 // 请求超时设置
+  timeout: 120000 // 请求超时设置
   //   withCredentials: false, // 跨域请求是否需要携带 cookie
 });
 // 创建请求拦截
