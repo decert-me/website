ee738bfd16de17bb47bb916c9b8b40bb049ae92f
Caoqizhi <2391580279@qq.com>
2023-08-14 10:46:26 +0800

    fix: percent 显示

--- src/views/Lesson.js
+++ src/views/Lesson.js
@@ -449,9 +449,8 @@ export default function Lesson(params) {
                                             </ul>
                                         </div>
                                         {
-                                            e?.percent && e.percent !== 0 &&
+                                            e?.percent != 0 &&
                                             <div className="progress">
-                                                {e.percent}
                                                 {t("progress")} {parseInt(e.percent * 100)}%
                                             </div>
                                         }
