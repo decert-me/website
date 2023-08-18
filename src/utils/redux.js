538954396f188f159d9f011e1d9b31f07fb7e065
Caoqizhi <2391580279@qq.com>
2023-08-18 10:55:12 +0800

    en: 强制刷新清除wagmicache

--- src/utils/redux.js
+++ src/utils/redux.js
@@ -4,5 +4,4 @@ import store, { setConnect } from "@/redux/store"
 export function changeConnect(params) {
     store.dispatch(setConnect(false))
     store.dispatch(setConnect(true))
-    localStorage.clear();
 }
