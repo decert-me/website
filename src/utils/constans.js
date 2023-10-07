6082c5b16263d92bb996ec599d8bf7ebc7f5f827
Caoqizhi <2391580279@qq.com>
2023-10-07 15:58:18 +0800

    fix: opensea链接错误

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -52,7 +52,7 @@ export const constans = (contractType) => {
             link: "https://gnosisscan.io/address/"
         }
     }
-    const defaultChainId = Number(process.env.REACT_APP_CHAIN_ID) || (window.location.hostname.indexOf("https://decert.me/") === -1 ? 80001 : 137);
+    const defaultChainId = Number(process.env.REACT_APP_CHAIN_ID) || (window.location.origin.indexOf("https://decert.me/") === -1 ? 80001 : 137);
 
     const questAddr = process.env.REACT_APP_CONTRACT_QUEST_ADDRESS;
 
@@ -61,8 +61,10 @@ export const constans = (contractType) => {
 
     const imgPath = process.env.REACT_APP_BASE_URL;
 
-    const openseaBase = window.location.hostname.indexOf("https://decert.me/") === -1 ? "https://testnets.opensea.io" : "https://opensea.io/";
-    const openseaLink = `${openseaBase}/assets/${chains[defaultChainId]?.opensea || chains[defaultChainId].name}/${contractType ? questAddr : BadgeAddress}`;
+    const openseaBase = window.location.origin.indexOf("https://decert.me/") === -1 ? "https://testnets.opensea.io" : "https://opensea.io/";
+    console.log(defaultChainId);
+    const openseaLink = `${openseaBase}/assets/${chains[defaultChainId]?.opensea || chains[defaultChainId]?.name}/${contractType ? questAddr : BadgeAddress}`;
+    console.log(openseaLink);
     const ipfsGateway = process.env.REACT_APP_IPFS_GATEWAY || "https://nftscan.mypinata.cloud/ipfs/"
     const defaultImg = require('@/assets/images/img/default.png');
     const ipfsPath = 'https://ipfs.decert.me';
