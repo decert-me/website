ebd528da67f20b569197402bda5f33b18f2ccbf8
Caoqizhi <2391580279@qq.com>
2023-10-08 09:00:27 +0800

    del: 删除测试代码

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -246,8 +246,7 @@ module.exports = function (webpackEnv) {
       level: 'none',
     },
     optimization: {
-      minimize: false,
-      // minimize: isEnvProduction,
+      minimize: isEnvProduction,
       minimizer: [
         // This is only used in production mode
         new TerserPlugin({
