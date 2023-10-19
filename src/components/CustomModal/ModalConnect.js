55499a86c146b0023b0df9a47c05ea30baac53c9
Caoqizhi <2391580279@qq.com>
2023-10-19 11:58:05 +0800

    fix: map error

--- src/components/CustomModal/ModalConnect.js
+++ src/components/CustomModal/ModalConnect.js
@@ -4,6 +4,7 @@ import { useIsMounted } from '@/hooks/useIsMounted'
 import "@/assets/styles/component-style"
 import { constans } from "@/utils/constans";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import React from "react";
 
 export default function ModalConnect(props) {
 
@@ -28,45 +29,67 @@ export default function ModalConnect(props) {
     }
 
     function Wallet(x, i) {
-        return (
-            <>
-            {
-                i === 1 && 
-                <WalletMultiButton>
-                    <div className="wallet-item">
+        if (i === 1) {
+            return (
+                <React.Fragment key={i}>
+                    <WalletMultiButton>
+                        <div className="wallet-item">
+                            <div className="item">
+                                <div className="img">
+                                    <img src={(require("@/assets/images/img/Solana.png"))} alt="" />
+                                </div>
+                                <p className="name">
+                                    Solana Wallet
+                                </p>
+                            </div>
+                        </div>
+                    </WalletMultiButton>
+                    <div
+                        key={x.name}
+                        className="wallet-item"
+                        disabled={!x.ready || isReconnecting || connector?.id === x.id}
+                        onClick={() => goConnect(x)}
+                    >
                         <div className="item">
                             <div className="img">
-                                <img src={(require("@/assets/images/img/Solana.png"))} alt="" />
+                                {
+                                    x.name === 'MetaMask' ? 
+                                        <img src={require("@/assets/images/img/MetaMask.png")} alt="" />
+                                        :
+                                        <img src={require("@/assets/images/img/WalletConnect.png")} alt="" />
+                                }
                             </div>
                             <p className="name">
-                                Solana Wallet
+                                {x.id === 'injected' ? (isMounted ? x.name : x.id) : x.name}
                             </p>
                         </div>
                     </div>
-                </WalletMultiButton>
-            }
-            <div
-                key={x.name}
-                className="wallet-item"
-                disabled={!x.ready || isReconnecting || connector?.id === x.id}
-                onClick={() => goConnect(x)}
-            >
-                <div className="item">
-                    <div className="img">
-                        {
-                            x.name === 'MetaMask' ? 
-                                <img src={require("@/assets/images/img/MetaMask.png")} alt="" />
-                                :
-                                <img src={require("@/assets/images/img/WalletConnect.png")} alt="" />
-                        }
+                </React.Fragment>
+            )
+        }else{
+            return (
+                <div
+                    key={x.name}
+                    className="wallet-item"
+                    disabled={!x.ready || isReconnecting || connector?.id === x.id}
+                    onClick={() => goConnect(x)}
+                >
+                    <div className="item">
+                        <div className="img">
+                            {
+                                x.name === 'MetaMask' ? 
+                                    <img src={require("@/assets/images/img/MetaMask.png")} alt="" />
+                                    :
+                                    <img src={require("@/assets/images/img/WalletConnect.png")} alt="" />
+                            }
+                        </div>
+                        <p className="name">
+                            {x.id === 'injected' ? (isMounted ? x.name : x.id) : x.name}
+                        </p>
                     </div>
-                    <p className="name">
-                        {x.id === 'injected' ? (isMounted ? x.name : x.id) : x.name}
-                    </p>
                 </div>
-            </div>
-            </>
-        )
+            )
+        }
     }
 
     return (
