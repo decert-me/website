d9332295d474f19e297e86b39928c4e559984896
Caoqizhi <2391580279@qq.com>
2023-11-23 17:20:08 +0800

    en: 调整ipfs上传超时时长

--- src/request/ipfs.js
+++ src/request/ipfs.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // Client-side-only code
 let ipfsAxios = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-  timeout: 60000 // 请求超时设置
+  timeout: 120000 // 请求超时设置
   //   withCredentials: false, // 跨域请求是否需要携带 cookie
 });
 // 创建请求拦截
