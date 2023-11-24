80024a6d8a210d0a09c1a111114847b999ce46b0
Caoqizhi <2391580279@qq.com>
2023-11-24 10:25:49 +0800

    en: 关闭严格模式

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { WalletPublic, WalletAdapter } from './wallet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <WalletAdapter>
         <WalletPublic>
@@ -21,7 +21,7 @@ root.render(
         </WalletPublic>
       </WalletAdapter>
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
