d5ca30635c17fc69c896c58a9e5abf86282c519f
Caoqizhi <2391580279@qq.com>
2023-12-27 10:49:24 +0800

    fix: 已提交编程题reload失败

--- src/components/CustomChallenge/CustomCode.js
+++ src/components/CustomChallenge/CustomCode.js
@@ -251,9 +251,9 @@ function CustomCode(props, ref) {
         setLogs([...logs]);
     }
 
-    async function init(params) {
+    async function init(isReload) {
         cacheQuest = question;
-        if (answers[index]) {
+        if (answers[index] && !isReload) {
             answerCode = answers[index].code;
             setAnswerCode(answerCode);
             cacheQuest.code_snippets[selectIndex].code = answers[index].code;
@@ -290,7 +290,7 @@ function CustomCode(props, ref) {
     }
 
     async function updateInit() {
-        await init();
+        await init(true);
         answerCode = null;
         setAnswerCode(answerCode);
         editorRef.current.monacoInit();
