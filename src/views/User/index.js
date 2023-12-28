e6d6a7dc483336520681e4e4467725c1e6a38219
Caoqizhi <2391580279@qq.com>
2023-12-28 11:07:39 +0800

    fix: 个人中心默认展示页初始化失败

--- src/views/User/index.js
+++ src/views/User/index.js
@@ -161,6 +161,9 @@ export default function User(props) {
                 checkType = Number(searchType);
                 setCheckType(checkType);
             }
+            if (searchStatus === null && searchType == 0) {
+                getList();
+            }
             return
         }
         checkType = location.search.indexOf("created") !== -1 ? 1 : 0;
