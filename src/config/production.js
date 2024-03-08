7bcb8678b5cd0138581f5b288d8c256eb66f3429
Caoqizhi <2391580279@qq.com>
2024-03-08 15:09:48 +0800

    en: add production config

--- src/config/production.js
+++ src/config/production.js
@@ -32,11 +32,11 @@ export const CONTRACT_ADDR_721 = {
     42161: {
         opensea: "arbitrum",
         img: require("@/assets/images/img/net-Arbitrum.png"),
-        Badge: "",
-        Quest: "",
-        BadgeMinter: "",
-        QuestMinter: "",
-        QuestMetadata: ""
+        Badge: "0x176A6AbBd7dFAd4C66d297f40269f910538212b7",
+        Quest: "0x373Dcc48Fa23451F792e604ba88c0bfFF17781c8",
+        BadgeMinter: "0x0aa319263401eEcecd5Fa2C34636b1057A8B2BFB",
+        QuestMinter: "0xE0E5Dc7709d339859DC2793e7afBd74EB29b108b",
+        QuestMetadata: "0xC7f03de02a2892E65e0019B8e5b58b6192C929A1"
     }
 }
 
