cf89f2dbcf0bd91cb0180d1991e3bb936235c913
unknown <2391580279@qq.com>
2024-07-28 16:56:24 +0800

    fix: claim-page annotation-list

--- src/views/Claim/info.js
+++ src/views/Claim/info.js
@@ -181,23 +181,26 @@ export default function ClaimInfo({answerInfo, detail}) {
                             }
                         </div>
                     </div>
-                    <div className="annotation">
-                        <p className="annotation-title">批注</p>
-                        <div className="annotation-list">
-                            {
-                                detail.answer.map((item, index) => (
-                                    item?.type === "open_quest" &&
-                                    <div key={index} className="annotation-item" onClick={() => navigateTo(`/challenge/${detail.uuid}?${index+1}`)}>
-                                        <div className="left">
-                                            <p>题目#{index+1}</p>
-                                            <p>{item.annotation}</p>
+                    {
+                        detail.answer.filter(e => e?.type === "open_quest").length !== 0 &&
+                        <div className="annotation">
+                            <p className="annotation-title">批注</p>
+                            <div className="annotation-list">
+                                {
+                                    detail.answer.map((item, index) => (
+                                        item?.type === "open_quest" &&
+                                        <div key={index} className="annotation-item" onClick={() => navigateTo(`/challenge/${detail.uuid}?${index+1}`)}>
+                                            <div className="left">
+                                                <p>题目#{index+1}</p>
+                                                <p>{item.annotation}</p>
+                                            </div>
+                                            <p>查看</p>
                                         </div>
-                                        <p>查看</p>
-                                    </div>
-                                ))
-                            }
+                                    ))
+                                }
+                            </div>
                         </div>
-                    </div>
+                    }
                     {
                         isMobile && 
                         <div className="mr">
